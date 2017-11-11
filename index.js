1e39676ef0db4a84bf73de81a62bfdbc2f5d912a
Malkuthe <jp.baltao@gmail.com>
2017-11-11 09:27:09 -0600

    included handling for empty yml files.

--- index.js
+++ index.js
@@ -15,140 +15,143 @@ function yaml2json(buffer, options) {
   var ymlOptions = {schema: options.schema, filename: options.filename};
   var ymlMonster = options.safe && contents != null ? yaml.safeLoad(contents, ymlOptions) : yaml.load(contents, ymlOptions);
   var monster = {};
-  monster.name = ymlMonster.name;
-  monster.size = firstCap(ymlMonster.size);
-  monster.type = firstCap(ymlMonster.type) + ( ymlMonster.subtype ? " (" + ymlMonster.subtype + ")" : "" );
-  monster.alignment = ymlMonster.alignment;
-  monster.AC = ymlMonster.ac + ( ymlMonster.armor ? " (" + ymlMonster.armor + ")" : "" );
-  monster.HP = ymlMonster.hp;
-  monster.speed = ymlMonster.speed;
-  monster.strength = ymlMonster.abilities.str;
-  monster.dexterity = ymlMonster.abilities.dex;
-  monster.constitution = ymlMonster.abilities.con;
-  monster.intelligence = ymlMonster.abilities.int;
-  monster.wisdom = ymlMonster.abilities.wis;
-  monster.charisma = ymlMonster.abilities.cha;
-  monster.challenge = ymlMonster.cr.toString();
 
-  if (ymlMonster.traits) {
-    monster.traits = [];
-    for (const prop in ymlMonster.traits) {
-      const trait = ymlMonster.traits[prop];
-      var traitObj = {}
-      traitObj.name = trait.name;
-      traitObj.text = removeMD(trait.description);
-      if (trait.uses) traitObj.recharge = trait.uses;
-      monster.traits.push(traitObj);
-    }
-  }
+  if (ymlMonster != null) {
+    monster.name = ymlMonster.name;
+    monster.size = firstCap(ymlMonster.size);
+    monster.type = firstCap(ymlMonster.type) + ( ymlMonster.subtype ? " (" + ymlMonster.subtype + ")" : "" );
+    monster.alignment = ymlMonster.alignment;
+    monster.AC = ymlMonster.ac + ( ymlMonster.armor ? " (" + ymlMonster.armor + ")" : "" );
+    monster.HP = ymlMonster.hp;
+    monster.speed = ymlMonster.speed;
+    monster.strength = ymlMonster.abilities.str;
+    monster.dexterity = ymlMonster.abilities.dex;
+    monster.constitution = ymlMonster.abilities.con;
+    monster.intelligence = ymlMonster.abilities.int;
+    monster.wisdom = ymlMonster.abilities.wis;
+    monster.charisma = ymlMonster.abilities.cha;
+    monster.challenge = ymlMonster.cr.toString();
 
-  if (ymlMonster.actions || ymlMonster.attacks) {
-    monster.actions = [];
-    if (ymlMonster.multiattack) {
-      monster.actions.push({
-        name: "Multiattack",
-        text: removeMD(ymlMonster.multiattack.description)
-      })
-    }
-
-    for (const prop in ymlMonster.actions) {
-      const action = ymlMonster.actions[prop];
-      var actionObj = {}
-      actionObj.name = action.name;
-      actionObj.text = removeMD(action.description);
-      if (action.uses) actionObj.recharge = action.uses;
-      monster.actions.push(actionObj);
-    }
-
-    for (const prop in ymlMonster.attacks) {
-      const attack = ymlMonster.attacks[prop];
-      var attackObj = {};
-      attackObj.name = attack.name;
-      attackObj.text = attack.type + ": ";
-      attackObj.text += ( attack.tohit >= 0 ? "+" : "-" ) + attack.tohit + " to hit, ";
-      attackObj.text += attack.reach ? "reach " + attack.reach : "";
-      attackObj.text += attack.reach && attack.range ? " or " : "";
-      attackObj.text += attack.range ? "range " + attack.range : "";
-      attackObj.text += ", ";
-      attackObj.text += attack.target + ". ";
-      attackObj.text += "Hit: " + attack.damage + ( attack.onhit ? " " + attack.onhit : "" );
-      if (attack.uses) attackObj.recharge = attack.uses;
-      monster.actions.push(attackObj);
-    }
-  }
-
-  if (ymlMonster.legendaryActions) {
-    monster.legendaryPoints = ymlMonster.legendaryPoints ? ymlMonster.legendaryPoints : 3;
-
-    monster.lActions = [];
-    for (const prop in ymlMonster.legendaryActions) {
-      if (prop == "description") {
-        continue;
+    if (ymlMonster.traits) {
+      monster.traits = [];
+      for (const prop in ymlMonster.traits) {
+        const trait = ymlMonster.traits[prop];
+        var traitObj = {}
+        traitObj.name = trait.name;
+        traitObj.text = removeMD(trait.description);
+        if (trait.uses) traitObj.recharge = trait.uses;
+        monster.traits.push(traitObj);
       }
-      const lAction = ymlMonster.legendaryActions[prop];
-      var lActionObj = {};
-      lActionObj.name = lAction.name;
-      lActionObj.text = removeMD(lAction.description);
-      lActionObj.cost = lAction.cost ? 1 : lAction.cost;
-      monster.lActions.push(lActionObj);
     }
-  }
 
-  if (ymlMonster.reactions) {
-    monster.reactions = [];
-    for (const prop in ymlMonster.reactions) {
-      const reaction = ymlMonster.reactions[prop];
-      var reactionObj = {};
-      reactionObj.name = reaction.name;
-      reactionObj.text = removeMD(reaction.description);
-      if (reaction.uses) reactionObj.recharge = reaction.uses;
-      monster.reactions.push(reactionObj);
+    if (ymlMonster.actions || ymlMonster.attacks) {
+      monster.actions = [];
+      if (ymlMonster.multiattack) {
+        monster.actions.push({
+          name: "Multiattack",
+          text: removeMD(ymlMonster.multiattack.description)
+        })
+      }
+
+      for (const prop in ymlMonster.actions) {
+        const action = ymlMonster.actions[prop];
+        var actionObj = {}
+        actionObj.name = action.name;
+        actionObj.text = removeMD(action.description);
+        if (action.uses) actionObj.recharge = action.uses;
+        monster.actions.push(actionObj);
+      }
+
+      for (const prop in ymlMonster.attacks) {
+        const attack = ymlMonster.attacks[prop];
+        var attackObj = {};
+        attackObj.name = attack.name;
+        attackObj.text = attack.type + ": ";
+        attackObj.text += ( attack.tohit >= 0 ? "+" : "-" ) + attack.tohit + " to hit, ";
+        attackObj.text += attack.reach ? "reach " + attack.reach : "";
+        attackObj.text += attack.reach && attack.range ? " or " : "";
+        attackObj.text += attack.range ? "range " + attack.range : "";
+        attackObj.text += ", ";
+        attackObj.text += attack.target + ". ";
+        attackObj.text += "Hit: " + attack.damage + ( attack.onhit ? " " + attack.onhit : "" );
+        if (attack.uses) attackObj.recharge = attack.uses;
+        monster.actions.push(attackObj);
+      }
     }
-  }
 
-  if (ymlMonster.saves) {
-    var savesArray = [];
-    for (var prop in ymlMonster.saves) {
-      var save = ymlMonster.saves[prop]
-      var temp = "";
-      temp += firstCap(prop);
-      temp += save >= 0 ? " +" + save : " -" + save;
-      savesArray.push(temp);
+    if (ymlMonster.legendaryActions) {
+      monster.legendaryPoints = ymlMonster.legendaryPoints ? ymlMonster.legendaryPoints : 3;
+
+      monster.lActions = [];
+      for (const prop in ymlMonster.legendaryActions) {
+        if (prop == "description") {
+          continue;
+        }
+        const lAction = ymlMonster.legendaryActions[prop];
+        var lActionObj = {};
+        lActionObj.name = lAction.name;
+        lActionObj.text = removeMD(lAction.description);
+        lActionObj.cost = lAction.cost ? 1 : lAction.cost;
+        monster.lActions.push(lActionObj);
+      }
     }
-    monster.savingThrows = savesArray.join(", ");
-  }
 
-  if (ymlMonster.skills) {
-    var skillsArray = [];
-    for (var prop in ymlMonster.skills) {
-      const skill = ymlMonster.skills[prop];
-      var temp = prop;
-      temp += skill >= 0 ? " +" + skill : " -" + skill;
-      skillsArray.push(temp);
+    if (ymlMonster.reactions) {
+      monster.reactions = [];
+      for (const prop in ymlMonster.reactions) {
+        const reaction = ymlMonster.reactions[prop];
+        var reactionObj = {};
+        reactionObj.name = reaction.name;
+        reactionObj.text = removeMD(reaction.description);
+        if (reaction.uses) reactionObj.recharge = reaction.uses;
+        monster.reactions.push(reactionObj);
+      }
     }
-    monster.skills = skillsArray.join(", ");
-  }
 
-  if (ymlMonster.condition) {
-    monster.conditionImmunities = ymlMonster.condition.immunities;
-  }
-
-  if (ymlMonster.damage) {
-    if (ymlMonster.damage.resistances) {
-      monster.damageResistances = ymlMonster.damage.resistances;
-    } else if (ymlMonster.damage.immunities) {
-      monster.damageImmunities = ymlMonster.damage.immunities;
-    } else if (ymlMonster.damage.vulnerabilities) {
-      monster.damageVulnerabilities = ymlMonster.damage.vulnerabilities;
+    if (ymlMonster.saves) {
+      var savesArray = [];
+      for (var prop in ymlMonster.saves) {
+        var save = ymlMonster.saves[prop]
+        var temp = "";
+        temp += firstCap(prop);
+        temp += save >= 0 ? " +" + save : " -" + save;
+        savesArray.push(temp);
+      }
+      monster.savingThrows = savesArray.join(", ");
     }
-  }
 
-  if (ymlMonster.senses) {
-    monster.senses = ymlMonster.senses;
-  }
+    if (ymlMonster.skills) {
+      var skillsArray = [];
+      for (var prop in ymlMonster.skills) {
+        const skill = ymlMonster.skills[prop];
+        var temp = prop;
+        temp += skill >= 0 ? " +" + skill : " -" + skill;
+        skillsArray.push(temp);
+      }
+      monster.skills = skillsArray.join(", ");
+    }
 
-  if (ymlMonster.languages) {
-    monster.languages = ymlMonster.languages;
+    if (ymlMonster.condition) {
+      monster.conditionImmunities = ymlMonster.condition.immunities;
+    }
+
+    if (ymlMonster.damage) {
+      if (ymlMonster.damage.resistances) {
+        monster.damageResistances = ymlMonster.damage.resistances;
+      } else if (ymlMonster.damage.immunities) {
+        monster.damageImmunities = ymlMonster.damage.immunities;
+      } else if (ymlMonster.damage.vulnerabilities) {
+        monster.damageVulnerabilities = ymlMonster.damage.vulnerabilities;
+      }
+    }
+
+    if (ymlMonster.senses) {
+      monster.senses = ymlMonster.senses;
+    }
+
+    if (ymlMonster.languages) {
+      monster.languages = ymlMonster.languages;
+    }
   }
 
   return new Buffer(JSON.stringify(monster, options.replacer, options.space));
