d98b90113ae4759392312e37b6f620b1797c85b1
Malkuthe <jp.baltao@gmail.com>
2018-02-11 10:46:41 -0600

    Testing Escape sequence

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function yaml2json(buffer, options) {
   var monster = {};
 
   if (ymlMonster != null) {
-    monster.name = ymlMonster.name.replace(/'/g,"\\'");
+    monster.name = ymlMonster.name.replace(/'/g,"&#39;");
     monster.size = firstCap(ymlMonster.size);
     monster.type = firstCap(ymlMonster.type) + ( ymlMonster.subtype ? " (" + ymlMonster.subtype + ")" : "" );
     monster.alignment = ymlMonster.alignment;
