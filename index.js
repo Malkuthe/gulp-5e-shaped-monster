fcdf0a4cc3d46fcda25f50ab4322a933fd78c209
Malkuthe <jp.baltao@gmail.com>
2018-07-20 19:24:05 -0500

    fixes

--- index.js
+++ index.js
@@ -40,18 +40,14 @@ function yaml2json(buffer, options) {
 
     // monster.challenge = ymlMonster.cr > 0 ? ymlMonster.cr.toString() : "0";
 
-    if (ymlMonster.cr) {
-      if (ymlMonster.cr == "1/8") {
-        monster.challenge = 0.125;
-      } else if (ymlMonster.cr == "1/4") {
-        monster.challenge = 0.25;
-      } else if (ymlMonster.cr == "1/2") {
-        monster.challenge = 0.50;
-      } else {
-        monster.challenge = ymlMonster.cr;
-      }
+    if (ymlMonster.cr == "1/8") {
+      monster.challenge = 0.125;
+    } else if (ymlMonster.cr == "1/4") {
+      monster.challenge = 0.25;
+    } else if (ymlMonster.cr == "1/2") {
+      monster.challenge = 0.50;
     } else {
-      monster.challenge = 0;
+      monster.challenge = ymlMonster.cr;
     }
 
     if (ymlMonster.traits) {
